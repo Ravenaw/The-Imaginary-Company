e87d2e3610bfceb20b89f875f326b040acc73e0e
Vlad F <vlad.faget@outlook.com>
2020-05-12 13:26:55 +0200

    Added a class library. Created a singleton

---
+++ InventoryLibrary/ClassLibrary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryLibrary
+{
+    public class Article
+    {
+        public int TIC { get; set; }
+        public int IAN { get; set; }
+        public string Owner { get; set; }
+        public int Quantity { get; set; }
+        public int Weight { get; set; }
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+    }
+
+    public class Somethingelse
+    {
+        public int TIC { get; set; }
+        public int IAN { get; set; }
+        public string Owner { get; set; }
+        public int Quantity { get; set; }
+        public int Weight { get; set; }
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+    }
+}
